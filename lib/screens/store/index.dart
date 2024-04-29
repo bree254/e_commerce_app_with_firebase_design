d35031390efadad7c1e4f4ac8d8f193d3d57b23c
Jared K Nyisang <nyisang70@gmail.com>
2024-04-29 15:32:50 +0300

    updated app themes

--- lib/screens/store/index.dart
+++ lib/screens/store/index.dart
@@ -1,4 +1,8 @@
+import 'package:e_commerce_firebase/utils/constants/colors.dart';
+import 'package:e_commerce_firebase/utils/constants/sizes.dart';
+import 'package:e_commerce_firebase/utils/helpers/helper_functions.dart';
 import 'package:e_commerce_firebase/widgets/common_widgets/cart_counter_icon_widget.dart';
+import 'package:e_commerce_firebase/widgets/common_widgets/search_container_widget.dart';
 import 'package:e_commerce_firebase/widgets/custom_widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 class StoreScreen extends StatelessWidget {
@@ -24,6 +28,29 @@ class StoreScreen extends StatelessWidget {
           headerSliverBuilder: (_,innerBoxIsScrolled){
             return [
             SliverAppBar(
+              automaticallyImplyLeading: false,
+              pinned: true,
+              floating: true,
+              backgroundColor: THelperFunctions.isDarkMode(context)?myColors.black:myColors.white,
+              expandedHeight: 440,
+              flexibleSpace: Padding(
+                  padding: EdgeInsets.all(TSizes.defaultSpace),
+                child: ListView(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  children: [
+                    SizedBox(height: TSizes.spaceBtwItems,),
+                    SearchContainer(
+                      text: 'Search in Store',
+                      showBorder: true,
+                      showBackground: false,
+                      //padding:EdgeInsets.zero,
+
+                    ),
+                    SizedBox(height: TSizes.spaceBtwItems,),
+                  ],
+                ),
+              ),
 
 
             ),
